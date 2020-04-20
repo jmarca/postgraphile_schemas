3d49520ff89b4c71a52243ef7d16cce5de5cc58c
James E. Marca <james@activimetrics.com>
2020-04-20 16:40:48 -0700

    need to revoke more stuff in order to get rid of :DATABASE_VISITOR

--- revert/permissions.sql
+++ revert/permissions.sql
@@ -2,6 +2,9 @@
 
 BEGIN;
 
+alter default privileges in schema public, app_public, app_hidden revoke usage, select on sequences from :DATABASE_VISITOR;
+alter default privileges in schema public, app_public, app_hidden revoke execute on functions from :DATABASE_VISITOR;
+
 grant all on schema public to public;
 alter default privileges grant all on sequences to public;
 alter default privileges grant all on functions to public;
